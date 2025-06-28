5a358fd1a266b7cc62ea26b36cf801ff0e2c35dd
Dervingo <tsepalkind@gmail.com>
2025-06-28 14:49:20 +0300

    Add files via upload

--- diplom/diplom_spo/models.py
+++ diplom/diplom_spo/models.py
@@ -2,14 +2,47 @@ from django.db import models
 
 
 class Diplom(models.Model):
-    org_name = models.CharField("Название образовательной организации", max_length=200)
-    qualification = models.CharField("Квалификация", max_length=100)
-    # ser_number = models.
-    student_name = models.CharField("ФИО студента", max_length=200)
-    title = models.CharField("Название диплома", max_length=300)
-    supervisor = models.CharField("Руководитель", max_length=200)
-    year = models.DateField("Год защиты")
-    description = models.TextField("Описание", blank=True)
+    org_name = models.CharField(verbose_name="Название образовательной организации",
+                                help_text="Введите навзвание образовательной организации",
+                                max_length=200,
+                                default='Введите данные')
+    qualification = models.CharField(max_length=100,
+                                     verbose_name="Квалификация",
+                                     help_text="Напишите квалификацию",
+                                     default='Введите данные')
+    ser_number = models.CharField(max_length=100,
+                                  verbose_name="Номер серии",
+                                  help_text="Введите номер серии",
+                                  default='Введите данные')
+    reg_number = models.CharField(max_length=10,
+                                  verbose_name="Регистрационный номер",
+                                  help_text="Введите регистрационный номер",
+                                  default='Введите данные')
+    grant_date = models.DateField(verbose_name="Дата выдачи",
+                                  help_text='Введите дату выдачи',
+                                  )
+    student_name = models.CharField(max_length=200,
+                                    verbose_name="ФИО",
+                                    help_text="ФИО",
+                                    default='Введите данные')
+    speciality = models.CharField(max_length=100,
+                                  verbose_name="Специальность",
+                                  help_text="Введите специальность",
+                                  default='Введите данные')
+    supervisor = models.CharField(max_length=200,
+                                  verbose_name="Руководитель",
+                                  help_text="Введите ФИО руководителя",
+                                  default='Введите данные')
+    chairman = models.CharField(max_length=200,
+                                verbose_name="Председатель ЭКЗ комиссии",
+                                help_text="Введите ФИО председатель",
+                                default='Введите данные')
+    year = models.DateField(verbose_name="Дата решения",
+                            help_text="Введите дату решения",
+                            )
+    copy = models.FileField(verbose_name="Копия диплома",
+                            help_text="Вставьте копию диплома",
+                            )
 
     class Meta:
         ordering = ('year',)
@@ -17,4 +50,4 @@ class Diplom(models.Model):
         verbose_name_plural = 'Дипломы'
 
     def __str__(self):
-        return f"{self.student_name} - {self.title} ({self.year})"
+        return f"{self.student_name} - ({self.year})"
